7bcf13bc64b21328d6965783325c04a260c96f7b
GPV <gvill005@gmail.com>
2023-06-21 14:15:58 -0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
+
+// CONFIG
 const app = express();
-dotenv.config()
+dotenv.config();
+app.use(express.json());
+app.use(helmet());
 
-
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
 
 app.listen(process.env.PORT, () => {
     console.log("listening on PORT", process.env.PORT || 9000);
